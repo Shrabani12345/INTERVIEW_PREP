bfeffc1c6385e234afd287b12826228fbfe1974c
Shrabani Mallick <mallickshrabani3@gmail.com>
2025-02-04 22:58:48 +0530

    edited

--- src/FORLOOP/ForLoop.java
+++ src/FORLOOP/ForLoop.java
@@ -3,15 +3,15 @@ package FORLOOP;
 public class ForLoop {
 
 	public static void main(String[] args) {
-		String str = "Sweety",rev="";
-		for(int i = str.length()-1; i>=0; i--) {
-			
-			char ch=str.charAt(i);
-			rev=rev+ch;
-			
+		String str = "Sweety", rev = "";
+		for (int i = str.length() - 1; i >= 0; i--) {
+
+			char ch = str.charAt(i);
+			rev = rev + ch;
+
 		}
-		System.out.println("rev str is :"+ rev);
-		
+		System.out.println("rev str is :" + rev);
+
 		// TODO Auto-generated method stub
 
 	}
